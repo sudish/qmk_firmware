8a50d7aeb6ac9fb5de3db2d8367c2cb342e94b15
Sudish Joseph <sudish@gmail.com>
2019-10-13 08:13:37 -0400

    Remove moribund USE_SERIAL_PD2 flag.

--- keyboards/crkbd/keymaps/sudish/config.h
+++ keyboards/crkbd/keymaps/sudish/config.h
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define SSD1306OLED
-#define USE_SERIAL_PD2
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
